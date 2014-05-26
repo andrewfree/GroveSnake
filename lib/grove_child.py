2c092689a5a22de0b4708a075700806d852d9f7d
Andrew Free <andrew@schjelderup.org>
2014-05-26 03:09:50 -0700

    Cleaned up code and changed how tagging was done. Mp3s keep their tagged info when the title doesn't look like an artist - title. Otherwise I rewrite the id3v2 tags.

--- lib/grove_child.py
+++ lib/grove_child.py
@@ -2,12 +2,15 @@
 # Docs https://github.com/kfdm/gntp/blob/master/docs/index.rst
 # import urllib2, httplib, json # Can get json metadata on youtube videos w/ http://gdata.youtube.com/feeds/api/videos/_7gcIbopIPk?v=2&alt=json
 # import soundcloud,client = soundcloud.Client(client_id='*',client_secret='*',username='*',password='*')
-# TO DO: Edge cases in title names like https://www.youtube.com/watch?v=iPLiLzt0ATo
+# TO DO: 
+# youtube open json api for more data "http://gdata.youtube.com/feeds/api/videos/%ID?alt=json&v=2" json["entry"]["author"][0]["name"]["$t"]
 import subprocess 
 import os
 import traceback
 import string
 import gntp.notifier
+import urllib2
+import simplejson
 
 def growlInit():
     growl = gntp.notifier.GrowlNotifier(
@@ -42,18 +45,23 @@ def main():
         env["PATH"] = "/sbin:/sbin:/usr/local/bin:/opt/local/bin:/opt/local/libexec/gnubin:/Users/rever/Documents/customBashExecute/:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/MacGPG2/bin:/Users/rever/.rvm/bin" #env["SHELL"] = '/bin/zsh'
         growl = growlInit()
         clipboard_link = ""
+        clipboard_provider = ""
         project_dir = ""
         meta_tagged = False
-        # processed = False
+        meta_flagged = False
         
         clipboard_process = subprocess.Popen("pbpaste", stdout=subprocess.PIPE)
         clipboard_link, err = clipboard_process.communicate()
-
 
         # Verify link being used is from soundcloud or youtube
         if not (("youtube" in clipboard_link) or ("soundcloud" in clipboard_link)):
             sendGrowlNotify(growl,"Bad Link!")
             os._exit(0)
+        else:
+            if ("youtube" in clipboard_link):
+                clipboard_provider = "youtube"
+            elif ("soundcloud" in clipboard_link):
+                clipboard_provider = "soundcloud"
 
         sendGrowlNotify(growl,"Downloading...")
 
@@ -65,9 +73,8 @@ def main():
         
         # Search if metadata already added.
         for item in output.split("\n"): # Iterate youtube-dl output line by line (after it finished running)
-            if (("JSON" in item) and ("soundcloud" in item)): # Search of soundcloud pulled data with song title-artist to be tagged by youtube-dl.
-                print "Found song metadata"
-                meta_tagged = True
+            if (("ffmpeg" in item) and ("metadata" in item)): # Search of soundcloud pulled data with song title-artist to be tagged by youtube-dl.
+                meta_tagged = True # If I want to mess with tags should read file from ffmpeg -i because youtube-dl sets that metadata first
 
         # Get files in tracks folder.
         track_list = os.listdir(os.getcwd())
@@ -81,18 +88,6 @@ def main():
             r_path = os.path.realpath(song)
             process_list.append((r_path,song)) # Fullpath and filename for ease.
 
-            # tag_list = subprocess.Popen(["/opt/local/bin/id3v2", "--list", song], stdout=subprocess.PIPE)
-            # for output_tags in tag_list.communicate():
-            #     if output_tags:
-            #         if ( ("TXXX" in output_tags) and ("done" in output_tags) ): # This file has been procesed by this program already, not adding to procces_list
-            #             processed = True
-            #     else:
-            #         continue
-
-            # # Has not been processed yet add to list       
-            # if processed != True:
-            #     process_list.append((r_path,song)) # Fullpath and filename for ease.
-
         if len(process_list) <= 0:
             sendGrowlNotify(growl,"No items to download.")
             os._exit(1)
@@ -102,31 +97,37 @@ def main():
         file_path = music_file[0]
         no_https_url = string.join(clipboard_link.split("/")[2:],"/") # Remove httpS out of url. Something about this url breaks OS X and metadata for finder items. 
         readable_size = readable_size_format(os.path.getsize(file_path))
-        artist,title = music_file[1].split("-")[0:2] # Used to tag youtube or soundcloud where metadata not avliable already.
+        music_file_id = music_file[1].split("-")[-1].split(".")[0] # Split filename and look at very end for song id since scheme has id.mp3
         
-        sendGrowlNotify(growl,"%s - %s" % (title,artist),code="%s" % readable_size, callback_url = clipboard_link, msg_type="Completed")
+        try:
+            title_without_mp3 = music_file[1][0:-len(music_file_id)-5]
+            artist,title = title_without_mp3.split("-")[0:2]
+        except ValueError:
+            meta_flagged = True
+            if clipboard_provider == "soundcloud":
+                url = "http://api.soundcloud.com/tracks/%s.json?client_id=" % music_file_id
+                json = simplejson.load(urllib2.urlopen(url))
+                title = title_without_mp3
+                artist = json["user"]['username'].encode('utf8')
+            elif clipboard_provider == "youtube":
+                url = "http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2" % music_file_id
+                json = simplejson.load(urllib2.urlopen(url))
+                artist = json["entry"]["author"][0]["name"]["$t"]
+                title = title_without_mp3
+        
         # Writing id3v2 comment tags with song url for lookup later. Soundcloud autowrites these tags so I don't tag for them.
-        if meta_tagged != True:
+        if meta_flagged != True:
             subprocess.Popen(["/opt/local/bin/id3v2", "-t", title, "-a", artist, "-c", no_https_url, music_file[1]],stdout=subprocess.PIPE)
-        else:
-            subprocess.Popen(["/opt/local/bin/id3v2", "-c", no_https_url, "%s" % file_path], stdout=subprocess.PIPE)
-        
-        # Tags special id3v2 field with done so it does not process again, as well as tagging Comments metadata field with "done" for programs like hazel to find. 
-        # subprocess.Popen(["/opt/local/bin/id3v2","--TXXX","done","%s" % file_path],stdout=subprocess.PIPE,env=env)
-        
+            #subprocess.Popen(["/opt/local/bin/xattr","-s","com.apple.metadata:kMDItemWhereFroms",no_https_url,music_file[1]],stdout=subprocess.PIPE)
+
         os.chdir(os.path.join(project_dir,"lib"))
         subprocess.Popen(["/opt/local/bin/bash","setFileComments.sh",file_path],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sendGrowlNotify(growl,"%s - %s" % (artist,title),code="%s" % readable_size, callback_url = clipboard_link, msg_type="Completed")
         os._exit(0)
-        # Added metadata from source, not usefull right now.
-        # subprocess.call(["xattr","-s","com.apple.metadata:kMDItemWhereFroms",provider,file_path],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     
     except Exception as e:
         print traceback.format_exc()
-        sendGrowlNotify(growl,'Not able to catch error, popen failed',msg_priority=1)
-        os._exit(1)
-    
-    except Exception, e: 
-        sendGrowlNotify(growl,"FAILED | %s" % e,msg_priority=1,msg_type="Completed",code="404")
+        sendGrowlNotify(growl,'Error: %s' % e,msg_priority=1)
         os._exit(1)
         
 if __name__ == "__main__":
