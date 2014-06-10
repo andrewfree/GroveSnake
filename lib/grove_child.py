f28d70e633ce752dbe667363ad6fdacfadd26d40
Andrew Free <andrew@schjelderup.org>
2014-06-10 14:42:53 -0700

    Changed tagging. Rewrites all tags no mater what with title artist and source url.

--- lib/grove_child.py
+++ lib/grove_child.py
@@ -2,8 +2,6 @@
 # Docs https://github.com/kfdm/gntp/blob/master/docs/index.rst
 # import urllib2, httplib, json # Can get json metadata on youtube videos w/ http://gdata.youtube.com/feeds/api/videos/_7gcIbopIPk?v=2&alt=json
 # import soundcloud,client = soundcloud.Client(client_id='*',client_secret='*',username='*',password='*')
-# TO DO: 
-# youtube open json api for more data "http://gdata.youtube.com/feeds/api/videos/%ID?alt=json&v=2" json["entry"]["author"][0]["name"]["$t"]
 import subprocess 
 import os
 import traceback
@@ -16,8 +14,7 @@ def growlInit():
     growl = gntp.notifier.GrowlNotifier(
         applicationName = "GroveSnake",
         notifications = ["Completed","New Download"],
-        defaultNotifications = ["New Download"]
-    )
+        defaultNotifications = ["New Download"])
     growl.register()
     return growl
 
@@ -30,8 +27,7 @@ def sendGrowlNotify(growl,message,callback_url="http://youtube.com",code="",msg_
         icon = "http://i.imgur.com/mXcrWGf.png", # http://i.imgur.com/cc0OCEo.png, http://i.imgur.com/Bfz3kgL.png?1
         sticky = False,
         priority = msg_priority,
-        callback = callback_url
-    )
+        callback = callback_url)
 
 def readable_size_format(num):
     for x in ['bytes','KB','MB','GB','TB']:
@@ -47,8 +43,7 @@ def main():
         clipboard_link = ""
         clipboard_provider = ""
         project_dir = ""
-        meta_tagged = False
-        meta_flagged = False
+        tagged = False
         
         clipboard_process = subprocess.Popen("pbpaste", stdout=subprocess.PIPE)
         clipboard_link, err = clipboard_process.communicate()
@@ -69,56 +64,59 @@ def main():
         os.chdir(os.path.join(project_dir,"tracks")) # Change into tracks folder for downloading.
         
         # Start download, max quality, safe filenames for handling below. The ID is in the filename so you can do metadata lookups for more info if wanted. Rips to mp3, might want to support native download formats, transcoding again and again lowers quality. 
-        output = subprocess.Popen(["/usr/local/bin/youtube-dl", "-o", "%(title)s-%(id)s.%(ext)s","--add-metadata", "-f","22/18/download/http_mp3_128_url", "--audio-format","mp3","--audio-quality", "0","-x",clipboard_link], stdout=subprocess.PIPE).communicate()[0] # stderr=subprocess.STDOUT,stdout=subprocess.PIPE
+        output = subprocess.Popen(["/usr/local/bin/youtube-dl", "-o", "%(title)s-%(id)s.%(ext)s","--add-metadata", "-f","22/18/download/http_mp3_128_url","--restrict-filenames","--audio-format","mp3","--audio-quality", "0","-x",clipboard_link], stdout=subprocess.PIPE).communicate()[0] # stderr=subprocess.STDOUT,stdout=subprocess.PIPE
         
-        # Search if metadata already added.
-        for item in output.split("\n"): # Iterate youtube-dl output line by line (after it finished running)
-            if (("ffmpeg" in item) and ("metadata" in item)): # Search of soundcloud pulled data with song title-artist to be tagged by youtube-dl.
-                meta_tagged = True # If I want to mess with tags should read file from ffmpeg -i because youtube-dl sets that metadata first
-
         # Get files in tracks folder.
-        track_list = os.listdir(os.getcwd())
-        track_list.remove(".DS_Store")
-        process_list = []
-        for song in track_list:
+        file_list = os.listdir(os.getcwd())
+        file_list.remove(".DS_Store")
+        song_list = []
+        for song in file_list:
             if song.split(".")[-1] != "mp3":
                 os.remove(song) # Have to do this until they fix bug in youtube-dl
                 continue
+            song_list.append((os.path.realpath(song),song)) # Fullpath and filename for ease.
 
-            r_path = os.path.realpath(song)
-            process_list.append((r_path,song)) # Fullpath and filename for ease.
-
-        if len(process_list) <= 0:
+        if len(song_list) <= 0:
             sendGrowlNotify(growl,"No items to download.")
             os._exit(1)
+        elif len(song_list) > 1:
+            sendGrowlNotify(growl,"Dirty tracks directory. Should only have a single song.")
+            os._exit(1)
 
-        # Write a lot of tags to stuff.                
-        music_file = process_list[0]
+        # File path variables & url / size of song / song id                
+        music_file = song_list[0]
         file_path = music_file[0]
         no_https_url = string.join(clipboard_link.split("/")[2:],"/") # Remove httpS out of url. Something about this url breaks OS X and metadata for finder items. 
         readable_size = readable_size_format(os.path.getsize(file_path))
         music_file_id = music_file[1].split("-")[-1].split(".")[0] # Split filename and look at very end for song id since scheme has id.mp3
+    
+        if clipboard_provider == "soundcloud":
+            url = "http://api.soundcloud.com/tracks/%s.json?client_id=key" % music_file_id
+            json = simplejson.load(urllib2.urlopen(url))
+            title = json["title"].strip()
+            artist = json["user"]['username'].encode('utf8').strip()
+            tagged = True # youtube-dl supports --add-metadata for this service
+        elif clipboard_provider == "youtube":
+            url = "http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2" % music_file_id
+            json = simplejson.load(urllib2.urlopen(url))
+            artist = json["entry"]["author"][0]["name"]["$t"].strip()
+            title = json["entry"]["title"]["$t"].strip()
+            tagged = True # youtube-dl supports --add-metadata for this service
         
-        try:
-            title_without_mp3 = music_file[1][0:-len(music_file_id)-5]
-            artist,title = title_without_mp3.split("-")[0:2]
-        except ValueError:
-            meta_flagged = True
-            if clipboard_provider == "soundcloud":
-                url = "http://api.soundcloud.com/tracks/%s.json?client_id=" % music_file_id
-                json = simplejson.load(urllib2.urlopen(url))
-                title = title_without_mp3
-                artist = json["user"]['username'].encode('utf8')
-            elif clipboard_provider == "youtube":
-                url = "http://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2" % music_file_id
-                json = simplejson.load(urllib2.urlopen(url))
-                artist = json["entry"]["author"][0]["name"]["$t"]
-                title = title_without_mp3
-        
-        # Writing id3v2 comment tags with song url for lookup later. Soundcloud autowrites these tags so I don't tag for them.
-        if meta_flagged != True:
+        dashes = title.count("-")
+        if dashes == 1:
+            artist = title.split("-")[0].strip()
+            title = title.split("-")[1].strip()
+            tagged = False # Above tags are more accurate, retag.
+        elif dashes > 1:
+            tagged = False # Re-write tags with response from api if song has more than one - in the title.
+        elif dashes == 0:
+            tagged = False # Testing re-writing all tags.
+
+        # Writing id3v2 comment tags with song url for lookup later.
+        if tagged != True:
             subprocess.Popen(["/opt/local/bin/id3v2", "-t", title, "-a", artist, "-c", no_https_url, music_file[1]],stdout=subprocess.PIPE)
-            #subprocess.Popen(["/opt/local/bin/xattr","-s","com.apple.metadata:kMDItemWhereFroms",no_https_url,music_file[1]],stdout=subprocess.PIPE)
+            #subprocess.Popen(["/opt/local/bin/xattr","-s","com.apple.metadata:kMDItemWhereFroms",no_https_url,music_file[1]],stdout=subprocess.PIPE) # For setting the Where From for Spotlight
 
         os.chdir(os.path.join(project_dir,"lib"))
         subprocess.Popen(["/opt/local/bin/bash","setFileComments.sh",file_path],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
